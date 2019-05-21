d551c345dded2d8d9c062db1f580b744c75c5856
Miguel Angel <devmiguelangel@outlook.com>
2019-05-21 12:00:06 -0400

    express mongodb connection

---
+++ ecommerce/lib/mongo.js
@@ -0,0 +1,73 @@
+const { MongoClient, ObjectId } = require("mongodb");
+
+const { config } = require("../config");
+
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+const DB_NAME = config.dbName;
+
+const MONGO_URL = `mongodb://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/?authSource=${DB_NAME}`; // prettier-ignore
+
+class MongoLib {
+  constructor() {
+    this.client = new MongoClient(MONGO_URL, { useNewUrlParser: true });
+    this.dbName = DB_NAME;
+  }
+
+  connect() {
+    return new Promise((resolve, reject) => {
+      this.client.connect(error => {
+        if (error !== null) {
+          reject(error);
+        }
+
+        console.log("Connected successfully to mongo");
+        resolve(this.client.db(this.dbName));
+      });
+    });
+  }
+
+  getAll(collection, query) {
+    return this.connect().then(db => {
+      return db
+        .collection(collection)
+        .find(query)
+        .toArray();
+    });
+  }
+
+  get(collection, id) {
+    return this.connect().then(db => {
+      return db.collection(collection).findOne({ _id: ObjectId(id) });
+    })
+  }
+
+  create(collection, data) {
+    return this.connect().then(db => {
+      return db.collection(collection).insertOne(data);
+    })
+    .then(result => result.insertedId);
+  }
+
+  update(collection, id, data) {
+    return this.connect()
+      .then(db => {
+        return db
+          .collection(collection)
+          .updateOne({ _id: ObjectId(id) }, { $set: data }, { upsert: true });
+      })
+      .then(result => result.updatedId || id)
+  }
+
+  delete(collection, id) {
+    return this.connect()
+      .then(db => {
+        return db
+          .collection(collection)
+          .deleteOne({ _id: ObjectId(id) });
+      })
+      .then(result => result.deletedId || id)
+  }
+}
+
+module.exports = MongoLib;
